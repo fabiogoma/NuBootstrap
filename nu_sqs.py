037459d10f25537dd4e775777ad11c4b6fb9852c
Fabio G. Martins <fabiogoma@gmail.com>
2016-11-14 21:13:20 -0200

    Created 2 SQS

--- nu_sqs.py
+++ nu_sqs.py
@@ -2,7 +2,21 @@
 # pylint: disable=C0111
 # pylint: disable=C0103
 # pylint: disable=C0301
+# pylint: disable=C0325
 
 import boto3
 
 client = boto3.client('sqs')
+
+response = client.create_queue(
+    QueueName='sqs_launch'
+)
+launch_queue_url = response['QueueUrl']
+
+response = client.create_queue(
+    QueueName='sqs_destroy'
+)
+destroy_queue_url = response['QueueUrl']
+
+print('Launch Queue URL: ' + launch_queue_url)
+print('Destroy Queue URL: ' + destroy_queue_url)
