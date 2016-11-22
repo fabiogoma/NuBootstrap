2b827a8a53058f2331c98d10e1734eb243288973
Fabio G. Martins <fabiogoma@gmail.com>
2016-11-21 23:10:18 -0200

    Added error handling

--- nu_ec2_describe_instance.py
+++ nu_ec2_describe_instance.py
@@ -24,6 +24,6 @@ try:
     print('Instance State: ' + instance_status)
     print('Public DNS: ' + public_name_dns)
 except botocore.exceptions.ClientError as e:
-    #if e.response['Error']['Code'] == 'InvalidGroup.Duplicate':
-    #    print('Group already exists')
-    print(e.response['Error']['Code'])
+    if e.response['Error']['Code'] == 'InvalidInstanceID.Malformed':
+        print('Invalid ID')
+
